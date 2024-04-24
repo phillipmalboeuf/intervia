4f3625491f4d7c2d9da83fdd175d9e862bcb9c9b
Phillip Malboeuf <phil@phils.computer>
2024-04-24 10:00:51 -0400

    csp

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,12 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// runtime: 'edge'
-		})
+		}),
+		csp: {
+			directives: {
+				'frame-ancestors': ['self', 'https://app.contentful.com']
+			}
+		}
 	}
 };
 
