f02af52f6c7c92e70b99df23909b87728c348e77
Phillip Malboeuf <phil@phils.computer>
2024-05-06 12:02:36 -0400

    ga

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,42 @@
 	import '../styles.scss'
 
 	import { page } from '$app/stores'
+	import { browser } from '$app/environment'
 	
 	import Header from '$lib/components/Header.svelte'
 	import Footer from '$lib/components/Footer.svelte'
   import Transition from '$lib/components/Transition.svelte'
   import Calendly from '$lib/components/Calendly.svelte'
+
+	const ga = 'G-4RGJEW3XW9'
+
+	if (browser) {
+		// @ts-ignore
+		window.dataLayer = window.dataLayer || []
+		// @ts-ignore
+		window.gtag = function gtag() {
+			// @ts-ignore
+			window.dataLayer.push(arguments)
+		}
+		// @ts-ignore
+		window.gtag("js", new Date())
+		// @ts-ignore
+		window.gtag("config", ga, { 'send_page_view': false })
+	}
+	$: {
+		// @ts-ignore
+		if (typeof gtag !== "undefined"){
+			// @ts-ignore
+			window.gtag("config", ga, {
+				page_path: $page.url.pathname,
+			})
+		}
+	}
 </script>
+
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id={ga}"></script>
+</svelte:head>
 
 <div class="app {$page.route.id.replaceAll('/', '-')} {$page.url.pathname.replaceAll('/', '-')} {{
 	'/projets': 'dark',
