23febf215c76e7334731a95d165bdf1193a2ef24
Phillip Malboeuf <phil@phils.computer>
2024-07-08 09:59:00 -0400

    contact button

--- src/lib/components/Calendly.svelte
+++ src/lib/components/Calendly.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import { browser } from '$app/environment'
+  
   import { onMount } from 'svelte'
 </script>
 
+
+{#if $page.url.pathname !== '/contact'}
+<a class="button" href="/contact">Parler à un expert {$page.url.pathname}</a>
+{:else}
 <button id="calendly" on:click={() => {
   // @ts-ignore
   window.Calendly?.initPopupWidget({url: 'https://calendly.com/interviainfo/30min?hide_gdpr_banner=1'})
-}}>Parler à un expert</button>
+}}>Prendre un rendez-vous</button>
+{/if}
 
 <style lang="scss">
+  .button,
   button {
     position: fixed;
     bottom: $gap;
